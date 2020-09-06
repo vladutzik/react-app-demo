dd48cdbb1037ba7711d357a2fdaef8e5a92a61bd
Vladimir Tribusean <vladimir.tribusean@gmail.com>
2020-09-06 17:21:39 +0200

    Updates

---
+++ precache-manifest.20cd3f1614ae7c0e49948d7f4b8a6db4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd103d916e6ad2895a7a6f2e31821564",
+    "url": "/react-app-demo/index.html"
+  },
+  {
+    "revision": "a2aaaba2fe8db3b1f1cd",
+    "url": "/react-app-demo/static/css/2.7b56354d.chunk.css"
+  },
+  {
+    "revision": "a2aaaba2fe8db3b1f1cd",
+    "url": "/react-app-demo/static/js/2.0a798652.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-app-demo/static/js/2.0a798652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa267baa1c5b1f6029e7",
+    "url": "/react-app-demo/static/js/main.5cd86a4f.chunk.js"
+  },
+  {
+    "revision": "d68b3b370ba9ce5e53c0",
+    "url": "/react-app-demo/static/js/runtime-main.b443c346.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
